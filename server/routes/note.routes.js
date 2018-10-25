40f6e04f82a9c3914fa689f324154da418fb6c93
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-10-25 20:49:49 +0100

    Edit/Delete lanes and notes

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -3,5 +3,11 @@ import * as NoteController from '../controllers/note.controller';
 
 const router = new Router();
 
+// Add a new Note
+router.route('/notes').post(NoteController.addNote);
+// Delete a Note by NoteId
+router.route('/notes/:noteId').delete(NoteController.deleteNote);
+// Edit a Note by NoteId
+router.route('/notes/:noteId').put(NoteController.editNote);
 
 export default router;
